6de2d949a6802e3115cdef968096510bd7c702e7
JoelGoose <joel.gasstrom@abo.fi>
2026-09-17 16:19:50 +0300

    exercise 3.12 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,23 @@
 const router = require('express').Router()
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
 
-const { Blog } = require('../models')
+const { Blog, User } = require('../models')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = JSON.parse(req.get('authorization'))
+
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch {
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  }  else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
@@ -11,22 +28,37 @@ const blogFinder = async (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()  
+  const blogs = await Blog.findAll({
+    attributes: { exclude: ['userId'] },
+    include: {
+      model: User
+    }
+  })  
   res.json(blogs)
 })
 
-router.post('/', async (req, res, next) => {
+router.post('/', tokenExtractor, async (req, res, next) => {
   try {
-    const blog = await Blog.create({...req.body})
+    const user = await User.findByPk(req.decodedToken.id)
+    const blog = await Blog.create({...req.body, userId: user.id})
     return res.json(blog)
   } catch(error) {
     next(error)
   }
 })
 
-router.delete('/:id', blogFinder, async (req, res) => {
-  await req.blog.destroy();
-  return res.status(204).end();
+router.delete('/:id', tokenExtractor, blogFinder, async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.decodedToken.id)
+    if (req.blog.userId === user.id) {
+      await req.blog.destroy();
+      return res.status(204).end();
+    } else {
+      return res.status(403).end();
+    }
+  } catch(error) {
+    next(error)
+  }
 })
 
 router.put('/:id', blogFinder, async (req, res, next) => {
--- util/middleware.js
+++ util/middleware.js
@@ -1,9 +1,18 @@
 const errorHandler = (error, req, res, next) => {
-  console.error('Error name:', error.name)
-  console.error('Error message:', error.message)
+  console.error(JSON.stringify(error))
 
   if (error.name === 'SequelizeValidationError') {
+    const emailError = error.errors.find(err => err.validatorKey === 'isEmail')
+    
+    if (emailError) {
+      return res.status(400).json({ error: "username must be a valid email address" })
+    }
+    
     return res.status(400).json({ error: error.message })
+  }
+
+  if (error.name === 'SequelizeUniqueConstraintError') {
+    return res.status(400).json({ error: error.errors[0].message })
   }
 
   return res.status(500).json({ error: 'Internal server error' })
