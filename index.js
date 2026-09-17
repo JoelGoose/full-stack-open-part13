875d7f7cfac67ea5aad7ae4811dc4b4874d1a19a
JoelGoose <joel.gasstrom@abo.fi>
2026-09-17 11:15:08 +0300

    exercise 3.7 done

--- index.js
+++ index.js
@@ -1,76 +1,31 @@
-require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogsRouter = require('./controllers/blogs')
+
+const errorHandler = (error, req, res, next) => {
+  console.error('Error name:', error.name)
+  console.error('Error message:', error.message)
+
+  if (error.name === 'SequelizeValidationError') {
+    return res.status(400).json({ error: error.message })
+  }
+
+  return res.status(500).json({ error: 'Internal server error' })
+}
+
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+app.use(errorHandler)
 
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()  
-  res.json(blogs)
-})
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-app.post('/api/blogs', async (req, res) => {
-  try {
-    const blog = await Blog.create({...req.body})
-    return res.json(blog)
-  } catch(error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.delete('/api/blogs/:id', async (req, res) => {
-  try {
-    const blogToDelete = await Blog.findByPk(req.params.id)
-    if (blogToDelete) {
-      await blogToDelete.destroy();
-      return res.status(204).end();
-    } else {
-      return res.status(404).json({ error: 'Blog not found' })
-    }
-  } catch(error) {
-    return res.status(500).json({ error })
-  }
-})
-
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false
-    }
-  },
-});
-
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
