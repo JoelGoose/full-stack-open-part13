3b366b8885e8802c04e90cb55a61d19465646d6c
JoelGoose <joel.gasstrom@abo.fi>
2026-09-18 12:08:13 +0300

    fix sync

--- models/index.js
+++ models/index.js
@@ -4,9 +4,11 @@ const User = require('./user')
 User.hasMany(Blog)
 Blog.belongsTo(User)
 
-Blog.sync({ alter: true })
-User.sync({ alter: true })
+const syncModels = async () => {
+  await User.sync({ alter: true })
+  await Note.sync({ alter: true })
+}
 
 module.exports = {
-  Blog, User
+  Blog, User, syncModels
 }
